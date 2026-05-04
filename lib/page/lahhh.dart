d008930439df5fef16182ab969072e45389d5ff4
WINDOWS 11 <nishaputriiii1812@gmail.com>
2026-05-04 19:26:43 +0700

    kerjakan tah

---
+++ lib/page/lahhh.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lahhh extends StatefulWidget {
+  const Lahhh({super.key});
+
+  @override
+  State<Lahhh> createState() => _LahhhState();
+}
+
+class _LahhhState extends State<Lahhh> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
